105f88514c9ca95e7a907654f921124512c23afd
praneethaedara <praneethaedara@gmail.com>
2019-06-13 19:08:08 +0530

    even number or odd number

--- src/org/tester/BaseClass.java
+++ src/org/tester/BaseClass.java
@@ -3,7 +3,13 @@ package org.tester;
 public class BaseClass {
 
 	public static void main(String[] args) {
-System.out.println("new class");
+		int a=12;
+		if(a%2==0) {
+System.out.println("even number");
+		}
+else {
+System.out.println("odd number");	
+}
 	}
 
 }
